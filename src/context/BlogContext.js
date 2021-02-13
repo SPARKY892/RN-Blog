87c7e55865c7784dffe44c58993b12547697f6d6
Mark <marksmellie892@gmail.com>
2021-02-13 19:08:16 +0000

    Simplified creating of reducers, implement context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([]);
-
-    const addBlogPost = () => {
-        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length +1}` }]);
+const blogReducer = (state, action) => {
+    switch(action.type) {
+        case 'delete_blogpost':
+            return state.filter((blogpost) => blogpost.id !== action.payload);
+        case 'add_blogpost':
+            return [
+                ...state, 
+                { 
+                    id: Math.floor(Math.random() * 99999),
+                    title: action.payload.title,
+                    content: action.payload.content
+                }];
+        default:
+            return state;
     }
-    
-    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }} >
-        {children}
-    </BlogContext.Provider>;
 };
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+    return (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: { title: title, content: content }});
+        callback();
+    };
+};
+
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
